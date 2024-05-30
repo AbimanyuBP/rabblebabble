c1c3d6166c04c4275abaec391bed5610b36501d2
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-05-30 11:50:30 +0700

    Finished Sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,52 +11,91 @@ import SchoolIcon from '@mui/icons-material/School';
 
 export default function Sidebar() {
   return (
-    <div className="grow-[3] h-[calc(100vh - 50px)]">
-      Sidebar
+    <div className=" flex-[3] h-[calc(100vh-50px)] overflow-y-scroll scrollbar-thin">
       <div className="p-[20px]">
-          <ul className="p-0 m-0 list-none *:flex *:items-center *:mb-[20px]">
-            <li className="">
+          <ul className="p-0 m-0 list-none">
+            <li className="flex items-center mb-[20px]">
               <RssFeedIcon className="mr-[15px]"/>
               <span className="">Feed</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <ChatIcon className="mr-[15px]"/>
               <span className="">Chats</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <PlayCircleIcon className="mr-[15px]"/>
               <span className="">Videos</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <Groups2Icon className="mr-[15px]"/>
               <span className="">Groups</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <BookmarksIcon className="mr-[15px]"/>
               <span className="">Bookmarks</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <HelpIcon className="mr-[15px]"/>
               <span className="">Questions</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <WorkIcon className="mr-[15px]"/>
               <span className="">Jobs</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <EventIcon className="mr-[15px]"/>
               <span className="">Events</span>
             </li>
-            <li className="">
+            <li className="flex items-center mb-[20px]">
               <SchoolIcon className="mr-[15px]"/>
               <span className="">Courses</span>
             </li>
           </ul>
-          <button className="">Show More</button>
-          <hr className="" />
-          <ul className="">
-            <li className="">
-              <img src="public\assets\person\1.jpeg" alt="" className="size-[32px]" />
+          <button className="w-[150px] bg-sky-600 p-[10px] rounded-[5px] font-medium">Show More</button>
+          <hr className="my-[20px] mx-0" />
+          <ul className="m-0 p-0 list-none">
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
+              <span className="">Jane Doe</span>
+            </li>
+            <li className="flex items-center mb-[15px]">
+              <img src="\assets\person\1.jpeg" alt="" className="size-[32px] rounded-[50%] object-cover mr-[10px]" />
               <span className="">Jane Doe</span>
             </li>
           </ul>
